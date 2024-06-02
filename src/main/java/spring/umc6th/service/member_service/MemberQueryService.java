ef51c2d3ab98bee5785a1519d649eca50de9b0d6
dabeann <dabin4078@daum.net>
2024-06-02 16:14:38 +0900

    feat: find member

--- src/main/java/spring/umc6th/service/member_service/MemberQueryService.java
+++ src/main/java/spring/umc6th/service/member_service/MemberQueryService.java
@@ -1,4 +1,9 @@
 package spring.umc6th.service.member_service;
 
+import java.util.Optional;
+import spring.umc6th.domain.Member;
+
 public interface MemberQueryService {
+
+    Optional<Member> findMember(Long id);
 }
--- src/main/java/spring/umc6th/service/member_service/MemberQueryServiceImpl.java
+++ src/main/java/spring/umc6th/service/member_service/MemberQueryServiceImpl.java
@@ -1,4 +1,21 @@
 package spring.umc6th.service.member_service;
 
-public class MemberQueryServiceImpl {
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.umc6th.domain.Member;
+import spring.umc6th.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberQueryServiceImpl implements MemberQueryService{
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Optional<Member> findMember(Long id) {
+        return memberRepository.findById(id);
+    }
 }
