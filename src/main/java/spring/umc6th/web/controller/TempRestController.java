589f95838371ee3359b6efa36e1cbabd02d693bd
dabeann <dabin4078@daum.net>
2024-06-02 14:46:32 +0900

    refactor: temp controller

--- src/main/java/spring/umc6th/web/controller/TempRestController.java
+++ src/main/java/spring/umc6th/web/controller/TempRestController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import spring.umc6th.base.Code;
-import spring.umc6th.base.ResponseDTO;
+import spring.umc6th.apiPayload.ApiResponse;
 import spring.umc6th.converter.TempConverter;
 import spring.umc6th.service.temp_service.TempQueryService;
-import spring.umc6th.web.dto.TempResponse;
+import spring.umc6th.web.dto.TempResponse.TempExceptionDTO;
 import spring.umc6th.web.dto.TempResponse.TempTestDTO;
 
 @RestController
@@ -20,14 +19,14 @@ public class TempRestController {
     private final TempQueryService tempQueryService;
 
     @GetMapping("/test")
-    public ResponseDTO<TempTestDTO> testAPI(){
+    public ApiResponse<TempTestDTO> testAPI(){
 
-        return ResponseDTO.onSuccess(TempConverter.toTempTestDTO(), Code.OK);
+        return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
     }
 
     @GetMapping("/exception")
-    public ResponseDTO<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){
+    public ApiResponse<TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){
         tempQueryService.CheckFlag(flag);
-        return ResponseDTO.onSuccess(TempConverter.toTempExceptionDTO(flag), Code.OK);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
     }
 }
