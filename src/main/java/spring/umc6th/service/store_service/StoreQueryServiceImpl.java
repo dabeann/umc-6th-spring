21ed31b86eb63175ec5cbc1fc0e96a1e90c3c299
dabeann <dabin4078@daum.net>
2024-06-02 16:15:32 +0900

    feat: find store

--- src/main/java/spring/umc6th/service/store_service/StoreQueryServiceImpl.java
+++ src/main/java/spring/umc6th/service/store_service/StoreQueryServiceImpl.java
@@ -1,11 +1,21 @@
 package spring.umc6th.service.store_service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import spring.umc6th.domain.Store;
+import spring.umc6th.repository.StoreRepository;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class StoreQueryServiceImpl implements StoreQueryService{
+
+    private final StoreRepository storeRepository;
+
+    @Override
+    public Optional<Store> findStore(Long id) {
+        return storeRepository.findById(id);
+    }
 }
