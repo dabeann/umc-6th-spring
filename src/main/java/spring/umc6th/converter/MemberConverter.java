5b4e8303f7e50986839ec1ccc796f6834fd4d0a0
dabeann <dabin4078@daum.net>
2024-06-02 14:42:25 +0900

    feat: member converter

---
+++ src/main/java/spring/umc6th/converter/MemberConverter.java
@@ -0,0 +1,43 @@
+package spring.umc6th.converter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import spring.umc6th.domain.Member;
+import spring.umc6th.domain.enums.Gender;
+import spring.umc6th.web.dto.MemberRequestDTO;
+import spring.umc6th.web.dto.MemberResponseDTO;
+
+public class MemberConverter {
+
+    public static MemberResponseDTO.JoinResultDTO toJoinResultDTO(Member member){
+        return MemberResponseDTO.JoinResultDTO.builder()
+                .memberId(member.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static Member toMember(MemberRequestDTO.JoinDto request) {
+
+        Gender gender = null;
+
+        switch (request.getGender()){
+            case 1:
+                gender = Gender.MALE;
+                break;
+            case 2:
+                gender = Gender.FEMALE;
+                break;
+            case 3:
+                gender = Gender.NONE;
+                break;
+        }
+
+        return Member.builder()
+                .address(request.getAddress())
+                .specAddress(request.getSpecAddress())
+                .gender(gender)
+                .name(request.getName())
+                .memberPreferList(new ArrayList<>())
+                .build();
+    }
+}
