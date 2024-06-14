c7c1fabeeb702c7236fe03f62d75593308941fca
dabeann <dabin4078@daum.net>
2024-06-14 13:50:46 +0900

    feat: review preview list dto

--- src/main/java/spring/umc6th/web/dto/StoreResponseDTO.java
+++ src/main/java/spring/umc6th/web/dto/StoreResponseDTO.java
@@ -1,6 +1,8 @@
 package spring.umc6th.web.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -33,5 +35,29 @@ public class StoreResponseDTO {
     public static class CreateMissionResultDTO {
         Long missionId;
         LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO{
+        List<ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO{
+        String ownerNickname;
+        Float score;
+        String body;
+        LocalDate createdAt;
     }
 }
