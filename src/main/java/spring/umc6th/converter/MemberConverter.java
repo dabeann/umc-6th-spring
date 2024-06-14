62142dd95eb4e83d44a6ce08813849e7e8c760bc
dabeann <dabin4078@daum.net>
2024-06-14 14:56:03 +0900

    feat: member converter with review

--- src/main/java/spring/umc6th/converter/MemberConverter.java
+++ src/main/java/spring/umc6th/converter/MemberConverter.java
@@ -2,10 +2,16 @@ package spring.umc6th.converter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 import spring.umc6th.domain.Member;
+import spring.umc6th.domain.Review;
 import spring.umc6th.domain.enums.Gender;
 import spring.umc6th.web.dto.MemberRequestDTO;
 import spring.umc6th.web.dto.MemberResponseDTO;
+import spring.umc6th.web.dto.StoreResponseDTO;
+import spring.umc6th.web.dto.StoreResponseDTO.ReviewPreViewDTO;
 
 public class MemberConverter {
 
@@ -38,6 +44,29 @@ public class MemberConverter {
                 .gender(gender)
                 .name(request.getName())
                 .memberPreferList(new ArrayList<>())
+                .build();
+    }
+
+    public static MemberResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return MemberResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getBody())
+                .build();
+    }
+
+    public static MemberResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+        List<ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return MemberResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
                 .build();
     }
 }
