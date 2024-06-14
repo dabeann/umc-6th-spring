29a6511a001bb36f136a95741071635bb8737261
dabeann <dabin4078@daum.net>
2024-06-14 14:55:44 +0900

    feat: find reviews by member service

--- src/main/java/spring/umc6th/service/member_service/MemberQueryService.java
+++ src/main/java/spring/umc6th/service/member_service/MemberQueryService.java
@@ -1,9 +1,13 @@
 package spring.umc6th.service.member_service;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
 import spring.umc6th.domain.Member;
+import spring.umc6th.domain.Review;
 
 public interface MemberQueryService {
 
     Optional<Member> findMember(Long id);
+
+    Page<Review> getReviewListByMemberId(Long memberId, Integer page);
 }
