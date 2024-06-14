2ddebfa8a93da6fe976c3a39e5e9a1d0a48938c0
dabeann <dabin4078@daum.net>
2024-06-14 13:52:21 +0900

    feat: review preview list converter

--- src/main/java/spring/umc6th/converter/StoreConverter.java
+++ src/main/java/spring/umc6th/converter/StoreConverter.java
@@ -1,6 +1,9 @@
 package spring.umc6th.converter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 import spring.umc6th.domain.Mission;
 import spring.umc6th.domain.Region;
 import spring.umc6th.domain.Review;
@@ -52,6 +55,29 @@ public class StoreConverter {
         return StoreResponseDTO.CreateMissionResultDTO.builder()
                 .missionId(mission.getId())
                 .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return StoreResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getBody())
+                .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+        List<StoreResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return StoreResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
                 .build();
     }
 }
