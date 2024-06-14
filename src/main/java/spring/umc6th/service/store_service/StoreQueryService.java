13e9aab5e09d6d3b7e56b2a0d1289cec08f1c2a9
dabeann <dabin4078@daum.net>
2024-06-14 13:51:53 +0900

    feat: get page review list in service

--- src/main/java/spring/umc6th/service/store_service/StoreQueryService.java
+++ src/main/java/spring/umc6th/service/store_service/StoreQueryService.java
@@ -1,9 +1,13 @@
 package spring.umc6th.service.store_service;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import spring.umc6th.domain.Review;
 import spring.umc6th.domain.Store;
 
 public interface StoreQueryService {
 
     Optional<Store> findStore(Long id);
+
+    Page<Review> getReviewList(Long storeId, Integer page);
 }
--- src/main/java/spring/umc6th/service/store_service/StoreQueryServiceImpl.java
+++ src/main/java/spring/umc6th/service/store_service/StoreQueryServiceImpl.java
@@ -2,9 +2,13 @@ package spring.umc6th.service.store_service;
 
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import spring.umc6th.domain.Review;
 import spring.umc6th.domain.Store;
+import spring.umc6th.repository.ReviewRepository;
 import spring.umc6th.repository.StoreRepository;
 
 @Service
@@ -13,9 +17,19 @@ import spring.umc6th.repository.StoreRepository;
 public class StoreQueryServiceImpl implements StoreQueryService{
 
     private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
 
     @Override
     public Optional<Store> findStore(Long id) {
         return storeRepository.findById(id);
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long storeId, Integer page) {
+
+        Store store = storeRepository.findById(storeId).get();
+
+        Page<Review> storePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return storePage;
     }
 }
