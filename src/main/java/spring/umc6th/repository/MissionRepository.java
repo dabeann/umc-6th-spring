fe471be60c24e0583144ca0df33a82ba331904d8
dabeann <dabin4078@daum.net>
2024-06-14 15:51:17 +0900

    feat: find mission by store

--- src/main/java/spring/umc6th/repository/MissionRepository.java
+++ src/main/java/spring/umc6th/repository/MissionRepository.java
@@ -1,7 +1,12 @@
 package spring.umc6th.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.umc6th.domain.Mission;
+import spring.umc6th.domain.Store;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+    Page<Mission> findAllByStore(Store store, PageRequest pageRequest);
 }
